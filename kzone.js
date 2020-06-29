3174d87883a971e773f5ea1a94dd44c3e5d8ca91
Andy McAdams <andymcadams23@gmail.com>
2020-06-29 18:40:01 -0500

    pushing rknfire

---
+++ kzone.js
@@ -0,0 +1,12 @@
+function startTracker(){
+    let pitchCounter = 0;
+    let strikeCounter = 0;
+    let ballCounter = 0;
+    let pitchPercentage = 0;
+    let streakTracker = 0;
+}
+
+
+function locationLogger(){
+
+}
